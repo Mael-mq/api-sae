f746ee9ff714595e2c418d03bac818bb91d29385
Maël Mouquet <mael.mqt@gmail.com>
2023-12-07 11:59:51 +0100

    installation serializer

--- src/Controller/InstrumentController.php
+++ src/Controller/InstrumentController.php
@@ -8,10 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class InstrumentController extends AbstractController
 {
-    #[Route('/instrument', name: 'app_instrument')]
-    public function index(): JsonResponse
+    #[Route('/api/instruments', name: 'api_instrument', methods: ['GET'])]
+    public function getInstruments(): JsonResponse
     {
-        return $this->json([
+        return new JsonResponse ([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/InstrumentController.php',
         ]);
