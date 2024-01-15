894302d87e03218715549f8d56bb81d515aca68d
Maël Mouquet <mael.mqt@gmail.com>
2024-01-15 15:15:22 +0100

    crud fichiers

--- project/src/Controller/FilesController.php
+++ project/src/Controller/FilesController.php
@@ -5,8 +5,8 @@ namespace App\Controller;
 use App\Entity\Cours;
 use App\Entity\Files;
 use App\Repository\CoursRepository;
+use App\Repository\FilesRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,15 +14,35 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Validator\Constraints\Url;
 
 class FilesController extends AbstractController
 {
+    #[Route('/api/files', name: 'api_files_list', methods: ['GET'])]
+    public function getFilesList(FilesRepository $filesRepository, CoursRepository $coursRepository, SerializerInterface $serializer, Request $request): JsonResponse
+    {
+        $content = $request->toArray();
+        $idCours = $content['idCours'] ?? -1;
+        $cours = $coursRepository->find($idCours);
+
+        $filesList = $filesRepository->findBy(['Cours'=>$cours]);
+        
+        $jsonFilesList = $serializer->serialize($filesList, 'json', ['groups' => 'files:read']);
+        return new JsonResponse ($jsonFilesList, Response::HTTP_OK, ['accept' => 'json'], true);
+    }
+
     #[Route('/api/files/{id}', name: 'api_files_detail', methods: ['GET'])]
     public function getFilesDetail(Files $files, SerializerInterface $serializer): JsonResponse
     {
         $jsonFiles = $serializer->serialize($files, 'json', ['groups' => 'files:read']);
         return new JsonResponse($jsonFiles, Response::HTTP_OK, ['accept' => 'json'], true);
+    }
+
+    #[Route('/api/files/{id}', name: 'api_files_delete', methods: ['DELETE'])]
+    public function deleteFiles(Files $files, EntityManagerInterface $em): JsonResponse
+    {
+        $em->remove($files);
+        $em->flush();
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 
     #[Route('/api/files', name: 'api_upload_files', methods: ['POST'])]
