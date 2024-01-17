7b957d8a6f3adc77c63042c1752fb4b3b5736d2f
Maël Mouquet <mael.mqt@gmail.com>
2024-01-17 10:53:36 +0100

    routes activities

--- project/src/Controller/ActivitiesController.php
+++ project/src/Controller/ActivitiesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Activities;
 use App\Repository\ActivitiesRepository;
+use App\Repository\CoursRepository;
 use App\Repository\SeanceRepository;
 use App\Repository\SheetRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -19,43 +20,51 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ActivitiesController extends AbstractController
 {
-    #[Route('/api/activities', name: 'api_activities', methods: ['GET'])]
-    public function getActivitiesList(ActivitiesRepository $activitiesRepository, SerializerInterface $serializer, Request $request): JsonResponse
+    #[Route('/api/cours/{idCours}/activities', name: 'api_activities', methods: ['GET'])]
+    public function getActivitiesList(CoursRepository $coursRepository, ActivitiesRepository $activitiesRepository, SerializerInterface $serializer, Request $request): JsonResponse
     {
-        $activitiesList = $activitiesRepository->findAll();
+        $cours = $coursRepository->find($request->get('idCours'));
+        $activitiesList = $activitiesRepository->findBy(['Cours' => $cours]);
         
         $jsonActivitiesList = $serializer->serialize($activitiesList, 'json', ['groups' => 'activities:read']);
         return new JsonResponse ($jsonActivitiesList, Response::HTTP_OK, ['accept' => 'json'], true);
     }
 
-    #[Route('/api/activities/{id}', name: 'api_activities_detail', methods: ['GET'])]
-    public function getActivitiesDetail(Activities $activities, SerializerInterface $serializer): JsonResponse
+    #[Route('/api/cours/{idCours}/activities/{idActivities}', name: 'api_activities_detail', methods: ['GET'])]
+    public function getActivitiesDetail(Request $request, ActivitiesRepository $activitiesRepository, CoursRepository $coursRepository, SerializerInterface $serializer): JsonResponse
     {
+        $activities = $activitiesRepository->find($request->get('idActivities'));
+        $cours = $coursRepository->find($request->get('idCours'));
+
         $jsonActivities = $serializer->serialize($activities, 'json', ['groups' => 'activities:read']);
         return new JsonResponse($jsonActivities, Response::HTTP_OK, ['accept' => 'json'], true);
     }
 
-    #[Route('/api/activities/{id}', name: 'api_activities_detail', methods: ['DELETE'])]
-    public function deleteActivities(Activities $activities, EntityManagerInterface $em): JsonResponse
+    #[Route('/api/cours/{idCours}/activities/{idActivities}', name: 'api_activities_detail', methods: ['DELETE'])]
+    public function deleteActivities(ActivitiesRepository $activitiesRepository, CoursRepository $coursRepository, Request $request, EntityManagerInterface $em): JsonResponse
     {
+        $activities = $activitiesRepository->find($request->get('idActivities'));
+        $cours = $coursRepository->find($request->get('idCours'));
+
         $em->remove($activities);
         $em->flush();
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 
-    #[Route('/api/activities', name: 'api_activities_create', methods: ['POST'])]
-    public function createActivities(Request $request, SerializerInterface $serializer, SeanceRepository $seanceRepository, SheetRepository $sheetRepository, EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator, ValidatorInterface $validator): JsonResponse
+    #[Route('/api/cours/{idCours}/activities', name: 'api_activities_create', methods: ['POST'])]
+    public function createActivities(Request $request, CoursRepository $coursRepository, SerializerInterface $serializer, SeanceRepository $seanceRepository, SheetRepository $sheetRepository, EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator, ValidatorInterface $validator): JsonResponse
     {
+        $cours = $coursRepository->find($request->get('idCours'));
+
         $activities = $serializer->deserialize($request->getContent(), Activities::class, 'json');
         
         $content = $request->toArray();
         $idSeance = $content['idSeance'] ?? -1;
         $idSheet = $content['idSheet'] ?? -1;
 
-        $seance = $seanceRepository->find($idSeance);
-        $activities->setSeance($seance);
+        $activities->setSeance($seanceRepository->find($idSeance));
         $activities->setSheet($sheetRepository->find($idSheet));
-        $activities->setCours($seance->getCours());
+        $activities->setCours($cours);
 
         // Validation des données
         $errors = $validator->validate($activities);
@@ -67,14 +76,17 @@ class ActivitiesController extends AbstractController
         $em->flush();
 
         $jsonActivities = $serializer->serialize($activities, 'json', ['groups' => 'activities:read']);
-        $location = $urlGenerator->generate('api_activities_detail', ['id' => $activities->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+        $location = $urlGenerator->generate('api_activities_detail', ['idActivities' => $activities->getId(), 'idCours' => $cours->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
 
         return new JsonResponse($jsonActivities, Response::HTTP_CREATED, ["Location" => $location], true);
     }
 
-    #[Route('/api/activities/{id}', name: 'api_activities_update', methods: ['PUT'])]
-    public function updateActivities(Request $request, SheetRepository $sheetRepository, SeanceRepository $seanceRepository, SerializerInterface $serializer, Activities $currentActivities, EntityManagerInterface $em, ValidatorInterface $validator): JsonResponse 
+    #[Route('/api/cours/{idCours}/activities/{idActivities}', name: 'api_activities_update', methods: ['PUT'])]
+    public function updateActivities(Request $request, SheetRepository $sheetRepository, SeanceRepository $seanceRepository, SerializerInterface $serializer, ActivitiesRepository $activitiesRepository, EntityManagerInterface $em, ValidatorInterface $validator): JsonResponse 
     {
+        $currentActivities = $activitiesRepository->find($request->get('idActivities'));
+        $cours = $currentActivities->getCours();
+
         $updatedActivities = $serializer->deserialize($request->getContent(), 
                 Activities::class, 
                 'json', 
@@ -88,7 +100,7 @@ class ActivitiesController extends AbstractController
         $seance = $seanceRepository->find($idSeance);
         $updatedActivities->setSeance($seance);
         $updatedActivities->setSheet($sheetRepository->find($idSheet));
-        $updatedActivities->setCours($seance->getCours());
+        $updatedActivities->setCours($cours);
         
         // Validation des données
         $errors = $validator->validate($updatedActivities);
