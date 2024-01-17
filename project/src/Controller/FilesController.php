f55fcebe0c1605d1d6a61c0e845db7b7a3cb4f1c
Maël Mouquet <mael.mqt@gmail.com>
2024-01-17 09:34:59 +0100

    modif routes files

--- project/src/Controller/FilesController.php
+++ project/src/Controller/FilesController.php
@@ -17,44 +17,52 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class FilesController extends AbstractController
 {
-    #[Route('/api/files', name: 'api_files_list', methods: ['GET'])]
+    #[Route('/api/cours/{idCours}/files', name: 'api_files_list', methods: ['GET'])]
     public function getFilesList(FilesRepository $filesRepository, CoursRepository $coursRepository, SerializerInterface $serializer, Request $request): JsonResponse
     {
-        $content = $request->toArray();
-        $idCours = $content['idCours'] ?? -1;
-        $cours = $coursRepository->find($idCours);
-
+        $cours = $coursRepository->find($request->attributes->get('idCours'));
         $filesList = $filesRepository->findBy(['Cours'=>$cours]);
         
         $jsonFilesList = $serializer->serialize($filesList, 'json', ['groups' => 'files:read']);
         return new JsonResponse ($jsonFilesList, Response::HTTP_OK, ['accept' => 'json'], true);
     }
 
-    #[Route('/api/files/{id}', name: 'api_files_detail', methods: ['GET'])]
-    public function getFilesDetail(Files $files, SerializerInterface $serializer): JsonResponse
+    #[Route('/api/cours/{idCours}/files/{idFiles}', name: 'api_files_detail', methods: ['GET'])]
+    public function getFilesDetail(FilesRepository $filesRepository, CoursRepository $coursRepository, SerializerInterface $serializer, Request $request): JsonResponse
     {
+        $files = $filesRepository->find($request->attributes->get('idFiles'));
+        $cours = $coursRepository->find($request->attributes->get('idCours'));
+
         $jsonFiles = $serializer->serialize($files, 'json', ['groups' => 'files:read']);
         return new JsonResponse($jsonFiles, Response::HTTP_OK, ['accept' => 'json'], true);
     }
 
-    #[Route('/api/files/{id}', name: 'api_files_delete', methods: ['DELETE'])]
-    public function deleteFiles(Files $files, EntityManagerInterface $em): JsonResponse
+    #[Route('/api/cours/{idCours}/files/{idFiles}', name: 'api_files_delete', methods: ['DELETE'])]
+    public function deleteFiles(FilesRepository $filesRepository, CoursRepository $coursRepository, Request $request, EntityManagerInterface $em): JsonResponse
     {
+        $files = $filesRepository->find($request->attributes->get('idFiles'));
+        $cours = $coursRepository->find($request->attributes->get('idCours'));
+        
         $em->remove($files);
         $em->flush();
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 
-    #[Route('/api/files', name: 'api_upload_files', methods: ['POST'])]
+    #[Route('/api/cours/{idCours}/files', name: 'api_upload_files', methods: ['POST'])]
     public function uploadFiles(Request $request, CoursRepository $coursRepository, EntityManagerInterface $em, SerializerInterface $serializer, UrlGeneratorInterface $urlGenerator)
     {
+        $cours = $coursRepository->find($request->attributes->get('idCours'));
         $files = new Files();
 
-        $idCours = $request->request->get('idCours') ?? -1;
-
-        $files->setCours($coursRepository->find($idCours));
+        $files->setCours($cours);
 
         $file = $request->files->get('file');
+
+        // Vérifier que l'extension du fichier est bien .pdf
+
+        if ($file->getClientOriginalExtension() != 'pdf') {
+            return new JsonResponse(null, Response::HTTP_BAD_REQUEST);
+        }
 
         $files->setUploadedFile($file);
 
